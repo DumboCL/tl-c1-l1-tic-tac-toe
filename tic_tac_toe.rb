41a300f4c2da5b744b8d40e6cfb3a20f41f8fef7
DumboCL <wyz513@gmail.com>
2015-01-06 11:38:37 +0800

    change the board data structure and modify the computer AI method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,6 +3,8 @@
 # Tealeaf Course 1 -- Lesson 1 Assignment
 # 29/12/2014
 
+require 'pry'
+
 PLAYER_SIGNAL = 'X'
 COMPUTER_SIGNAL = 'O'
 
@@ -24,40 +26,32 @@ def continue_next(choose)
 end
 
 def input_validation(board_status,position_choose) 
-  positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  if positions.include?(position_choose)
-    if position_choose % 3 == 0
-      board_status[position_choose/3-1][2] == ' '
-    else
-      board_status[position_choose/3][position_choose%3-1] == ' '
-    end
+
+  if board_status.keys.include?(position_choose)
+    board_status[position_choose] == ' '
   else
     return false
   end
 end
 
 def board_initial
-  board_status = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  
-  for i in 0..2 do
-    for j in 0..2 do
-      board_status[i][j] = ' '
-    end
-  end
+  board_status = {}  
+  (1..9).each{|position| board_status[position] = ' '}
   board_status
 end
 
 def draw_board(board_status)
   system 'clear'
   puts "     |     |     "
-  puts "  #{board_status[0][0]}  |  #{board_status[0][1]}  |  #{board_status[0][2]}   "
+  puts "  #{board_status[1]}  |  #{board_status[2]}  |  #{board_status[3]}   "
   puts "     |     |     "
   puts "-----+-----+-----"
   puts "     |     |     "
-  puts "  #{board_status[1][0]}  |  #{board_status[1][1]}  |  #{board_status[1][2]}   "
+  puts "  #{board_status[4]}  |  #{board_status[5]}  |  #{board_status[6]}   "
   puts "     |     |     "
   puts "-----+-----+-----"
   puts "     |     |     "
-  puts "  #{board_status[2][0]}  |  #{board_status[2][1]}  |  #{board_status[2][2]}   "
+  puts "  #{board_status[7]}  |  #{board_status[8]}  |  #{board_status[9]}   "
   puts "     |     |     "
   puts
 end
@@ -73,45 +67,21 @@ def winner_annoucement(signal)
 end
 
 def has_result(board_status)
-  # Horizontal line
-  for i in 0..2 do
-    if board_status[i][0] == ' '
-      next
-    end      
-    if board_status[i][0] == board_status[i][1] && board_status[i][0] == board_status[i][2]
-      winner_annoucement(board_status[i][0])
+  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  winning_lines.each do |line|
+    if board_status[line[0]] == PLAYER_SIGNAL and board_status[line[1]] == PLAYER_SIGNAL and board_status[line[2]] == PLAYER_SIGNAL
+      winner_annoucement(PLAYER_SIGNAL)
+      return true
+    elsif board_status[line[0]] == COMPUTER_SIGNAL and board_status[line[1]] == COMPUTER_SIGNAL and board_status[line[2]] == COMPUTER_SIGNAL
+      winner_annoucement(COMPUTER_SIGNAL)
       return true
     end
-  end
-
-  # Vertical line
-  for i in 0..2 do
-    if board_status[0][i] == ' '
-      next
-    end      
-    if board_status[0][i] == board_status[1][i] && board_status[0][i] == board_status[2][i]
-      winner_annoucement(board_status[0][i])
-      return true
-    end
-  end
-  
-  # Diagonal
-  if board_status[0][0] != ' ' && (board_status[0][0] == board_status[1][1] && board_status[0][0] == board_status[2][2])
-    winner_annoucement(board_status[0][0])
-    return true
-  end
-
-  if board_status[0][2] != ' ' && (board_status[0][2] == board_status[1][1] && board_status[0][2] == board_status[2][0])
-    winner_annoucement(board_status[0][2])
-    return true
   end
 
   tie = true
-  for i in 0..2 do
-    for j in 0..2 do
-      if board_status[i][j] == ' '
-        tie = false
-      end
+  board_status.each do |key,value|
+    if value == ' '
+      tie = false
     end
   end
 
@@ -124,12 +94,10 @@ def has_result(board_status)
 end
 
 def who_is_first
-
   say "Press ENTER to see who will be the first to play?"
   i = 0
   first_player = ' '
-  who_is_first_thread = Thread.new do
-    
+  who_is_first_thread = Thread.new do    
     while true
       if i % 2 == 0 
         first_player = "You     "
@@ -144,7 +112,6 @@ def who_is_first
       i += 1
       sleep 0.1
     end
-
   end
 
   gets
@@ -154,230 +121,35 @@ def who_is_first
     return "You"
   else
     return "Computer"
-  end
- 
+  end 
 end
 
+# checks to see if two in a row
 def computer_AI_choose(board_status)
-  player_win_position = 0
-  # i is the target position
-  for i in 1..9 do
-    case i
-    when 1
-      if board_status[0][0] != ' '
-        next
-      end
-      if board_status[0][1] == board_status[0][2] && board_status[0][1] != ' '
-        if board_status[0][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][1] == board_status[2][2] && board_status[1][1] != ' '
-        if board_status[1][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][0] == board_status[2][0] && board_status[1][0] != ' '
-        if board_status[1][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 2
-      if board_status[0][1] != ' '
-        next
-      end
-      if board_status[0][0] == board_status[0][2] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][1] == board_status[2][1] && board_status[1][1] != ' '
-        if board_status[1][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 3
-      if board_status[0][2] != ' '
-        next
-      end
-      if board_status[0][0] == board_status[0][1] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][2] == board_status[2][2] && board_status[1][2] != ' '
-        if board_status[1][2] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][1] == board_status[2][0] && board_status[1][1] != ' '
-        if board_status[1][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 4
-      if board_status[1][0] != ' '
-        next
-      end
-      if board_status[0][0] == board_status[2][0] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[1][1] == board_status[1][2] && board_status[1][1] != ' '
-        if board_status[1][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 5
-      if board_status[1][1] != ' '
-        next
-      end
-      if board_status[1][0] == board_status[1][2] && board_status[1][0] != ' '
-        if board_status[1][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][1] == board_status[2][1] && board_status[0][1] != ' '
-        if board_status[0][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][0] == board_status[2][2] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][2] == board_status[2][0] && board_status[0][2] != ' '
-        if board_status[0][2] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 6
-      if board_status[1][2] != ' '
-        next
-      end
-      if board_status[1][0] == board_status[1][1] && board_status[1][0] != ' '
-        if board_status[1][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][2] == board_status[2][2] && board_status[0][2] != ' '
-        if board_status[0][2] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 7
-      if board_status[2][0] != ' '
-        next
-      end
-      if board_status[2][1] == board_status[2][2] && board_status[2][1] != ' '
-        if board_status[2][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][0] == board_status[1][0] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][2] == board_status[1][1] && board_status[0][2] != ' '
-        if board_status[0][2] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 8
-      if board_status[2][1] != ' '
-        next
-      end
-      if board_status[2][0] == board_status[2][2] && board_status[2][0] != ' '
-        if board_status[2][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][1] == board_status[1][1] && board_status[0][1] != ' '
-        if board_status[0][1] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-    when 9
-      if board_status[2][2] != ' '
-        next
-      end
-      if board_status[2][0] == board_status[2][1] && board_status[2][0] != ' '
-        if board_status[2][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][2] == board_status[1][2] && board_status[0][2] != ' '
-        if board_status[0][2] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end
-      if board_status[0][0] == board_status[1][1] && board_status[0][0] != ' '
-        if board_status[0][0] == PLAYER_SIGNAL
-          player_win_position = i
-        else
-          return i
-        end
-      end      
+  winning_lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  # first try to win
+  winning_lines.each do |line|
+    test_line = {line[0] => board_status[line[0]],line[1] => board_status[line[1]], line[2] => board_status[line[2]]}
+    if test_line.values.count(COMPUTER_SIGNAL) == 2 and test_line.values.count(' ') == 1
+      return test_line.select{ |k,v| v == ' ' }.keys.first
     end
   end
-  if player_win_position > 0
-    return player_win_position
+
+  # second try to stop player wins
+  winning_lines.each do |line|
+    test_line = {line[0] => board_status[line[0]],line[1] => board_status[line[1]], line[2] => board_status[line[2]]}
+    if test_line.values.count(PLAYER_SIGNAL) == 2 and test_line.values.count(' ') == 1
+      return test_line.select{ |k,v| v == ' ' }.keys.first
+    end
   end
 
+  # last make a random choise
   random_number = Random.new
   return random_number.rand(1..9)
-end
-# main process
 
+end
+
+# main process
 begin
   board_status = board_initial
   draw_board(board_status)   
@@ -386,13 +158,14 @@ begin
   if first_player == 'You'
     player_turn = true
   else
-    player_turn = false
-    
+    player_turn = false    
   end
+  
   # game begin
   begin
     draw_board(board_status)
     say "The first player is: #{first_player}"
+    #binding.pry
     if player_turn
       # player's turn
       begin                 
@@ -402,22 +175,18 @@ begin
     else
       # computer's turn
       begin
+        say "Computer's turn"
         position_choose = computer_AI_choose(board_status)
+        puts position_choose
       end while !input_validation(board_status, position_choose)
     end
     
     # setting piece
-    if position_choose % 3 == 0
-      board_status[position_choose/3-1][2] = player_turn ? PLAYER_SIGNAL : COMPUTER_SIGNAL
-    else
-      board_status[position_choose/3][position_choose%3-1] = player_turn ? PLAYER_SIGNAL : COMPUTER_SIGNAL   
-    end
-    
+    board_status[position_choose] = player_turn ? PLAYER_SIGNAL : COMPUTER_SIGNAL
+   
     # change roles
     player_turn = !player_turn
-
     draw_board(board_status)
-
   end while !has_result(board_status)
 
   # validate input
